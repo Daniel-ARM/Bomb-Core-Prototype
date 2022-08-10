4b82b7fc7676eb148444ddbd95d2a485a882330b
Daniel-ARM <82000192+Daniel-ARM@users.noreply.github.com>
2022-08-10 03:52:51 -0300

    Patch Notes: - Spawn Randômico de Itens e Inimigos (todos tipos) por sala, baseado em Grid

--- Assets/Scripts/ItemSpawner.cs
+++ Assets/Scripts/ItemSpawner.cs
@@ -38,7 +38,7 @@ public class ItemSpawner : MonoBehaviour
             cumulativeWeight += items[chosenIndex].weight;
         }
 
-        GameObject i = Instantiate(items[chosenIndex].gameObject, transform.position, Quaternion.identity) as GameObject;
+        GameObject i = Instantiate(items[chosenIndex].gameObject, transform.position, Quaternion.identity, transform) as GameObject;
 
     }
 
